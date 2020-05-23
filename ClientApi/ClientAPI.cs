c2c39ab452c6bfeaf088f4534ddc057dfa4a1689
strilciuc_gabriel98 <strilciuc_gabriel98@yahoo.ro>
2020-05-23 14:24:18 +0300

    api client definition

--- ClientApi/ClientAPI.cs
+++ ClientApi/ClientAPI.cs
@@ -1,8 +1,22 @@
-﻿
-namespace ClientApi
+﻿namespace ClientApi
 {
     public class ClientAPI
     {
+        private ClientAPI instance = null;
+        private const string apiURL = "https://api.spoonacular.com";
+        private const string apiKey = "apiKey=7b137a9d00974ba4b92d25f4d51d11c6";
+        private ClientAPI()
+        {
 
+        }
+
+        public ClientAPI getInstance()
+        {
+            if(instance == null)
+            {
+                instance = new ClientAPI();
+            }
+            return instance;
+        }
     }
 }

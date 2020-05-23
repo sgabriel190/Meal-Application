0ae77a92d3153c8ac336355eeff80cf88f44b0d6
strilciuc_gabriel98 <strilciuc_gabriel98@yahoo.ro>
2020-05-23 20:25:03 +0300

    update clientapi

--- ClientApi/ClientAPI.cs
+++ ClientApi/ClientAPI.cs
@@ -1,13 +1,29 @@
-﻿namespace ClientApi
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace ClientApi
 {
+    /**
+     *  Clasa ClientAPI contine un client HTTP care permite aplicatiei sa comunice cu serverul API 
+     * folosidu-i functiile puse la dispozitie pentru a dispune de datele necesare.
+     */
     public class ClientAPI
     {
         private ClientAPI instance = null;
         private const string apiURL = "https://api.spoonacular.com";
         private const string apiKey = "apiKey=7b137a9d00974ba4b92d25f4d51d11c6";
+        private HttpClient httpClient = null;
+        //viitor parser pentru request string
+
         private ClientAPI()
         {
-
+            //Instantiere si setari client HTTP
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new System.Uri(apiURL);
+            httpClient.DefaultRequestHeaders
+                .Accept
+                .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            
         }
 
         public ClientAPI getInstance()

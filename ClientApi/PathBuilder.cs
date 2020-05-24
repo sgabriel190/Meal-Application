7b935db16b1a79431273e64b941c55aa2c279eae
strilciuc_gabriel98 <strilciuc_gabriel98@yahoo.ro>
2020-05-25 00:57:31 +0300

    update api

--- ClientApi/PathBuilder.cs
+++ ClientApi/PathBuilder.cs
@@ -1,6 +1,13 @@
 ﻿
 namespace ClientApi
 {
+    /**
+     * Clasa PathBuilder este o clasa helper pentru ClientApi in care se construieste 
+     * resursa apelata la serverul api-ului
+     * EX: pentru a primi un JSON cu toate informatiile despre id-ul retetei 100 se apeleaza 
+     * recipes/100/information?apiKey=7b137a9d00974ba4b92d25f4d51d11c6 care va intoarce intr-un
+     * raspuns HTTP un obiect JSON cu toate informatiile necesare.
+     */
     public class PathBuilder
     {
         public string buildPathIngredients(string apiKey, string ingredientsQuery, int numberRecipes)

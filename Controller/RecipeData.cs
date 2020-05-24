e35f7a873946ec6e7cd40e6b08a88bf18d8f02d5
Panainte Ancuta <ancuta.panainte@student.tuiasi.ro>
2020-05-24 16:18:44 +0300

    added class RecipeData

---
+++ Controller/RecipeData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Controller
+{
+    class RecipeData
+    {
+        private int _id;
+        private string _title;
+        private Bitmap _img;
+        private string _description;
+        private string _url;
+
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+        public Bitmap Image
+        {
+            get { return _img; }
+            set { _img = value; }
+        }
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        public string URL
+        {
+            get { return _url; }
+            set { _url = value; }
+        }
+    }
+}

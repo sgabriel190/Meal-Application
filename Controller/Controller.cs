73d7026c925b27001ccdf2306bd10333eca13407
strilciuc_gabriel98 <strilciuc_gabriel98@yahoo.ro>
2020-05-25 00:41:22 +0300

    update controller

--- Controller/Controller.cs
+++ Controller/Controller.cs
@@ -1,8 +1,21 @@
-﻿
+﻿using ClientApi;
+using Newtonsoft.Json.Linq;
+
 namespace Controller
 {
+    /**
+     * Clasa controller detine acces direct care toate functionalitatile aplicatiei,
+     * prin urmare este singura clasa care comunica cu interfata si dirijeaza functionalitatile
+     * claselor proiectate pentru backend.
+     */
     public class Controller
     {
+        private ClientAPI clientAPI = null;
+
+        public Controller()
+        {
+            clientAPI = ClientAPI.getInstance();
+        }
 
     }
 }

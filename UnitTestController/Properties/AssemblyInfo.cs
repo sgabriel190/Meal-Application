cb10962a3746bb00dd3eb94775e6325f76a6bd04
strilciuc_gabriel98 <strilciuc_gabriel98@yahoo.ro>
2020-05-26 21:39:30 +0300

    added unit testing

---
+++ UnitTestController/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestController")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestController")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7a640094-5c48-4f6c-b58d-5a1c81555051")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestController/UnitTestContriollerClass.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ControllerNamespace;
+using System.Collections.Generic;
+using DataModel;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTestControllerClass
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Controller controller = new Controller();
+            List<RecipeData> listRecipes1 = controller.GetRecipiesFromIngridients("ingredients=apples,+flour,+sugar", 2);
+            List<RecipeData> listRecipes2 = controller.GetRecipiesFromIngridients("ingredients=appless,+flouR,+sugar32", 2);
+
+            Assert.AreEqual(listRecipes1,listRecipes2, "ingredients1: apples flour sugar \n ingredients: appless flouR sugar32");
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Controller controller = new Controller();
+            List<RecipeData> listRecipes1 = controller.GetRecipiesFromIngridients("apples, flour, sugar", 2);
+            List<RecipeData> listRecipes2 = controller.GetRecipiesFromIngridients("appless, flouR, sugar32", 2);
+
+            Assert.AreEqual(listRecipes1, listRecipes2, "ingredients1: apples, flour, sugar \n ingredients2: appless, flouR, sugar32");
+
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Controller controller = new Controller();
+            List<RecipeData> listRecipes1 = controller.GetRecipiesFromIngridients("cheese", 2);
+            List<RecipeData> listRecipes2 = controller.GetRecipiesFromIngridients("cheese", 2);
+
+            Assert.AreEqual(listRecipes1, listRecipes2, "ingredients1: cheese \n ingredients2: cheese");
+
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Controller controller = new Controller();
+            List<RecipeData> listRecipes1 = controller.GetRecipiesFromIngridients("bread, cheese", 2);
+            List<RecipeData> listRecipes2 = controller.GetRecipiesFromIngridients("bread,cheese", 2);
+
+            Assert.AreEqual(listRecipes1, listRecipes2, "ingredients1: bread, cheese \n ingredients2: bread,cheese");
+
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Controller controller = new Controller();
+            List<RecipeData> listRecipes1 = controller.GetRecipiesFromIngridients("bread, cheese", 2);
+            List<RecipeData> listRecipes2 = controller.GetRecipiesFromIngridients("bread cheese", 2);
+
+            Assert.AreEqual(listRecipes1, listRecipes2, "ingredients1: bread, cheese \n ingredients2: bread cheese");
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Controller controller = new Controller();
+            List<RecipeData> listRecipes1 = controller.GetRecipiesFromIngridients("bread, cheese", 2);
+            List<RecipeData> listRecipes2 = controller.GetRecipiesFromIngridients("bread, cheesssse", 2);
+
+            Assert.AreEqual(listRecipes1, listRecipes2, "ingredients1: bread, cheese \n ingredients2: bread, cheesssse");
+        }
+    }
+}

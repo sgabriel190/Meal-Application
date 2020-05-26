ce21886b8d8f0a4208485434d3efe58c171404c1
strilciuc_gabriel98 <strilciuc_gabriel98@yahoo.ro>
2020-05-26 20:22:16 +0300

    update api code

--- ClientApi/ClientAPI.cs
+++ ClientApi/ClientAPI.cs
@@ -2,6 +2,11 @@
 using System.Net.Http;
 using System.Net.Http.Headers;
 using Newtonsoft.Json.Linq;
+/***
+ * Clasa ClientAPI implementeaza un client HTTP care sa se ocupe de comunicatia cu serverul HTTP oferit de API.
+ * Tot in aceasta clasa se incearca indentificare erorilor aparute prin urmarea accesarii serverului HTTP.
+ * Autor: Strilciuc Gabriel
+ ***/
 
 namespace ClientApi
 {
@@ -12,11 +17,14 @@ namespace ClientApi
     public class ClientAPI
     {
         private static ClientAPI _instance = null;
+
         private const string _apiURL = "https://api.spoonacular.com";
         private string _apiKey = "apiKey=a3a2299445e34aef82762e4fa925d413";
+
         private HttpClient _httpClient = null;
         private PathBuilder _pathBuilder = null;
 
+        // Constructor privat pentru realizarea sablonului de singleton
         private ClientAPI()
         {
             //Instantiere si setari client HTTP
@@ -30,6 +38,7 @@ namespace ClientApi
             
         }
        
+        // Metoda publica pentru a accesa unica instanta de clientAPI din aplicatie
         public static ClientAPI GetInstance()
         {
             if(_instance == null)
@@ -38,28 +47,57 @@ namespace ClientApi
             }
             return _instance;
         }
+        // Metoda setter pentru atributul apiKey
         public string ApiKey
         {
             set { _apiKey = "apiKey=" + value; }
         }
+
+        /**
+         * Metoda SearchByIngredients primeste ca parametru un query URI si foloseste clientul HTTP 
+         * definit local in clasa pentru a apela functiile API-ului prin protocolul HTTP.
+         *** Query-ul contine parametrii necesari functiei de cautare bazata pe ingrediente.
+         * Aceasta clasa returneaza un JArray, continut de raspunsul HTTP primit de la API.
+         */
         public JArray SearchByIngredients(string ingredientQuery)
         {
             string path = _pathBuilder.BuildPathIngredients(_apiKey, ingredientQuery);
             HttpResponseMessage response = _httpClient.GetAsync(path).Result;
             return ReturnJsonArrayFromResponse(response);
         }
+
+        /**
+         * Metoda SearchByNutrients primeste ca parametru un query URI si foloseste clientul HTTP 
+         * definit local in clasa pentru a apela functiile API-ului prin protocolul HTTP.
+         *** Query-ul contine parametrii necesari functiei de cautare bazata pe nutrienti.
+         * Aceasta clasa returneaza un JArray, continut de raspunsul HTTP primit de la API.
+         */
         public JArray SearchByNutrients(string nutrientsQuery)
         {
             string path = _pathBuilder.BuildPathNutrients(_apiKey, nutrientsQuery);
             HttpResponseMessage response = _httpClient.GetAsync(path).Result;
             return ReturnJsonArrayFromResponse(response);
         }
+
+        /**
+         * Metoda GenerateMealPlan primeste ca parametru un query URI si foloseste clientul HTTP 
+         * definit local in clasa pentru a apela functiile API-ului prin protocolul HTTP.
+         *** Query-ul contine parametrii necesari functiei de generare a unui plan alimentar bazat pe un numar de parametrii.
+         * Aceasta clasa returneaza un JObject, continut de raspunsul HTTP primit de la API.
+         */
         public JObject GenerateMealPlan(string mealPlannerQuery)
         {
             string path = _pathBuilder.BuildPathGenerateMealPlan(_apiKey, mealPlannerQuery);
             HttpResponseMessage response = _httpClient.GetAsync(path).Result;
             return ReturnJsonObjectFromResponse(response);
         }
+
+        /**
+         * Metoda GetById primeste ca parametru un query URI si foloseste clientul HTTP 
+         * definit local in clasa pentru a apela functiile API-ului prin protocolul HTTP.
+         *** Query-ul contine parametrii necesari functiei de returnare a unei retete complete, bazat pe id.
+         * Aceasta clasa returneaza un JObject, continut de raspunsul HTTP primit de la API.
+         */
         public JObject GetById(int id)
         {
             string path = _pathBuilder.BuildPathInformationId(_apiKey, id);
@@ -67,6 +105,11 @@ namespace ClientApi
             return ReturnJsonObjectFromResponse(response);
         }
 
+        /**
+         * Metoda ReturnJsonArrayFromResponse primeste ca parametru raspunsul oferit de catre API aplicatiei
+         * si pe baza acesuia, daca codul HTTP este unul de succes, se extrage din continut un array JSON.
+         * Acest array este trimis ulterior aplicatiei pentru a pregatii datele datele necesare.
+         */
         private JArray ReturnJsonArrayFromResponse(HttpResponseMessage response)
         {
             JArray json = null;
@@ -82,6 +125,12 @@ namespace ClientApi
             }
             return json;
         }
+
+        /**
+         * Metoda ReturnJsonObjectFromResponse primeste ca parametru raspunsul oferit de catre API aplicatiei
+         * si pe baza acesuia, daca codul HTTP este unul de succes, se extrage din continut un obiect JSON.
+         * Acest obiect este trimis ulterior aplicatiei pentru a pregatii datele datele necesare.
+         */
         private JObject ReturnJsonObjectFromResponse(HttpResponseMessage response)
         {
             JObject json = null;
@@ -98,6 +147,10 @@ namespace ClientApi
             return json;
         }
 
+        /**
+         * Metoda CheckErrorMessage este o functie de tip helper. Aceasta incearca sa decida, in cazul in care raspunsul 
+         * HTTP nu are un cod de succes 2xx, in care dintre situatiile de eroare se afla si trimite o exceptie catre interfata.
+         */
         private void CheckErrorMessage(HttpResponseMessage response)
         {
             if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
--- ClientApi/PathBuilder.cs
+++ ClientApi/PathBuilder.cs
@@ -1,45 +1,49 @@
-﻿
+﻿/**
+* Clasa PathBuilder este o clasa helper pentru ClientApi in care se construieste 
+* resursa apelata la serverul api-ului
+* EX: pentru a primi un JSON cu toate informatiile despre id-ul retetei 100 se apeleaza 
+* recipes/100/information?apiKey=7b137a9d00974ba4b92d25f4d51d11c6 care va intoarce intr-un
+* raspuns HTTP un obiect JSON cu toate informatiile necesare.
+* Autor: Strilciuc Gabriel
+*/
 namespace ClientApi
 {
-    /**
-     * Clasa PathBuilder este o clasa helper pentru ClientApi in care se construieste 
-     * resursa apelata la serverul api-ului
-     * EX: pentru a primi un JSON cu toate informatiile despre id-ul retetei 100 se apeleaza 
-     * recipes/100/information?apiKey=7b137a9d00974ba4b92d25f4d51d11c6 care va intoarce intr-un
-     * raspuns HTTP un obiect JSON cu toate informatiile necesare.
-     */
     public class PathBuilder
     {
-        // Metoda care construieste resursa specifica functiei searchByIngredients
+        // Metoda care construieste resursa specifica functiei SearchByIngredients
         public string BuildPathIngredients(string apiKey, string ingredientsQuery)
         {
             string path = "recipes/findByIngredients";
             path += "?" + apiKey + "&" + ingredientsQuery;
             return path;
         }
-        // Metoda care construieste resursa specifica functiei searchByNutrients
+
+        // Metoda care construieste resursa specifica functiei SearchByNutrients
         public string BuildPathNutrients(string apiKey, string nutrientsQuery)
         {
             string path = "recipes/findByNutrients";
             path += "?" + apiKey + "&" + nutrientsQuery;
             return path;
         }
-        // Metoda care construieste resursa specifica functiei getRecipeInformation
+
+        // Metoda care construieste resursa specifica functiei GetRecipeInformation
         public string BuildPathInformationId(string apiKey, int id)
         {
             string path = $"recipes/{id}/information";
             path += "?" + apiKey + "&includeNutrition=true";
             return path;
         }
+
+        // Metoda care construieste resursa specifica functiei GenerateMealPlan
         public string BuildPathGenerateMealPlan(string apiKey, string mealPlannerQuery)
         {
             string path = "mealplanner/generate";
+            //se adauga parametrul timeFrame=day pentru a avea constant planul alimentar pentru o zi
             path += "?" + apiKey + "&timeFrame=day";
-            if(mealPlannerQuery != "")
+            if (mealPlannerQuery != "")
             {
                 path += mealPlannerQuery;
             }
-            
             return path;
         }
     }
